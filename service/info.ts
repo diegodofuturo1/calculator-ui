a83a7ed7c8fadbd6cfe34abea3cca145749a04d4
Diego Heleno <diegodofuturo1@gmail.com>
2022-05-16 04:29:43 -0300

    fixed: api url

--- service/info.ts
+++ service/info.ts
@@ -1,6 +1,6 @@
 const config = {
-    path: 'http://localhost:8081',
-    // path: 'http://calculatorapienv-env.eba-mmg8pmtn.sa-east-1.elasticbeanstalk.com',
+    // path: 'http://localhost:8081',
+    path: 'http://calculatorapienv-env.eba-mmg8pmtn.sa-east-1.elasticbeanstalk.com',
 }
 
 export default config
